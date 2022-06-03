69800733c75f516b043eee9e5587c54bd66bb962
Gonzalo Mansilla Torre <93394668+VanGon1992@users.noreply.github.com>
2022-06-03 16:51:10 +0200

    Added App main folder and index.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import PostApp from "./components/PostApp";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <PostApp />
+    </div>
+  );
+}
